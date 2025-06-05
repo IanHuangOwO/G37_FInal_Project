468bc381712595082c1652dd60f3db41a49410b9
yuchen <candy301.wu@gmail.com>
2025-06-05 16:04:58 +0800

    modified:   character/guodong.c

--- character/guodong.c
+++ character/guodong.c
@@ -1,4 +1,8 @@
 #include "guodong.h"
+#include "../projectiles/baby.h"
+#include "../projectiles/tornado.h"
+#include "../projectiles/ice_wall.h"
+
 
 void GuoDong_Load_Assets(Character *chara) {
     // Load animations
@@ -94,8 +98,8 @@ void _GuoDong_Attack_2(Elements *self) {
     float angle_deg = chara->atk_angle;
     float power = chara->atk_power;
 
-    int x = chara->x + chara->width / 2 - TARIFF_WIDTH / 2;
-    int y = chara->y + chara->height / 2 - TARIFF_HEIGHT / 2;
+    int x = chara->x + chara->width / 2 - ICE_WALL_WIDTH / 2;
+    int y = chara->y + chara->height / 2 - ICE_WALL_HEIGHT / 2;
 
     if (!chara->dir) angle_deg = 180.0f - angle_deg;
 
